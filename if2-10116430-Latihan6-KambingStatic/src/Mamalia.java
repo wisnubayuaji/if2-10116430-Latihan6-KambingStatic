786ddf2821a2c0d399f868066ead70e1df5d4c52
wisnubayuaji <wisnubayu34@gmail.com>
2020-10-26 09:48:44 +0700

    rapihin folder

--- if2-10116430-Latihan6-KambingStatic/src/Mamalia.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author
- * Nim  : 10116430
- * Nama : Wisnu Bayu Aji
- * Kelas: IF-2
- */
-public class Mamalia {
-    public static int jumlahKambing;
-}
---
+++ src/Mamalia.java
@@ -0,0 +1,9 @@
+/**
+ * @author
+ * Nim  : 10116430
+ * Nama : Wisnu Bayu Aji
+ * Kelas: IF-2
+ */
+public class Mamalia {
+    public static int jumlahKambing;
+}
